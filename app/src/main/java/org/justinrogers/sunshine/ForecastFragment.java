83afebe26daf92597f3be433b745ac279dad53c6
yaskyj <yaskyj@yahoo.com>
2015-06-12 21:30:07 -0500

    refactor: Add user location setting for fetch weather task

--- app/src/main/java/org/justinrogers/sunshine/ForecastFragment.java
+++ app/src/main/java/org/justinrogers/sunshine/ForecastFragment.java
@@ -2,8 +2,10 @@ package org.justinrogers.sunshine;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.text.format.Time;
@@ -64,7 +66,9 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("70130");
+            SharedPreferences sharePref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String locationPref = sharePref.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
+            weatherTask.execute(locationPref);
             return true;
         }
         return super.onOptionsItemSelected(item);
