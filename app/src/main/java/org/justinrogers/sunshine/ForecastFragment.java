e3a78c9002f3cb7edc33cf357231d3cf2d9e756b
yaskyj <yaskyj@yahoo.com>
2015-06-12 19:17:32 -0500

    Feat: Add intent to open DetailActivity on click

--- app/src/main/java/org/justinrogers/sunshine/ForecastFragment.java
+++ app/src/main/java/org/justinrogers/sunshine/ForecastFragment.java
@@ -1,6 +1,7 @@
 package org.justinrogers.sunshine;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.v4.app.Fragment;
@@ -103,7 +104,9 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String forecast = mForecastAdapter.getItem(position);
-                Toast.makeText(getActivity(), forecast, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getActivity(), DetailActivity.class)
+                        .putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
             }
         });
 
