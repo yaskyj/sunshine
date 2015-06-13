08b16f43a775c2cf201040840663e56b820c9c58
yaskyj <yaskyj@yahoo.com>
2015-06-12 20:02:27 -0500

    Feat: Add forecast text to fragment_detail from intent

--- app/src/main/java/org/justinrogers/sunshine/DetailActivity.java
+++ app/src/main/java/org/justinrogers/sunshine/DetailActivity.java
@@ -1,13 +1,16 @@
 package org.justinrogers.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -57,7 +60,16 @@ public class DetailActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
 
-            View rootView = inflater.inflate(R.layout.activity_detail, container, false);
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            // The detail Activity called via intent.  Inspect the intent for forecast data.
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
+
             return rootView;
         }
     }
