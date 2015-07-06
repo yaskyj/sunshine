3409b2d8647066ab9faf578de35e8364cba3ff8b
yaskyj <yaskyj@yahoo.com>
2015-07-06 08:18:15 -0500

    refactor: Add service for updating weather and remove FetchWeatherTask

--- app/src/main/java/org/justinrogers/sunshine/ForecastFragment.java
+++ app/src/main/java/org/justinrogers/sunshine/ForecastFragment.java
@@ -18,6 +18,7 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import org.justinrogers.sunshine.data.WeatherContract;
+import org.justinrogers.sunshine.service.SunshineService;
 
 
 /**
@@ -166,9 +167,10 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
     }
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
-        String location = Utility.getPreferredLocation(getActivity());
-        weatherTask.execute(location);
+        Intent intent = new Intent(getActivity(), SunshineService.class);
+        intent.putExtra(SunshineService.LOCATION_QUERY_EXTRA,
+                Utility.getPreferredLocation(getActivity()));
+        getActivity().startService(intent);
     }
 
     @Override
