cd8b34ad3e7924164d5e9b920b069e92717b2a01
yaskyj <yaskyj@yahoo.com>
2015-08-15 14:15:27 -0500

    refactor: Create custom preference with custom attribute

---
+++ app/src/main/java/org/justinrogers/sunshine/LocationEditTextPreference.java
@@ -0,0 +1,24 @@
+package org.justinrogers.sunshine;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.preference.EditTextPreference;
+import android.util.AttributeSet;
+
+public class LocationEditTextPreference extends EditTextPreference {
+    static final private int DEFAULT_MINIMUM_LOCATION_LENGTH = 2;
+    private int mMinLength;
+
+    public LocationEditTextPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.LocationEditTextPreference,
+                0, 0);
+        try {
+            mMinLength = a.getInteger(R.styleable.LocationEditTextPreference_minLength, DEFAULT_MINIMUM_LOCATION_LENGTH);
+        } finally {
+            a.recycle();
+        }
+    }
+}
