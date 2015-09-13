c3fb047ce4496e4a9b57bacf063c45a36f516d05
yaskyj <yaskyj@yahoo.com>
2015-09-13 12:31:18 -0500

    refactor: Allow the widget to resize horizontally

--- app/src/main/java/org/justinrogers/sunshine/widget/TodayWidgetProvider.java
+++ app/src/main/java/org/justinrogers/sunshine/widget/TodayWidgetProvider.java
@@ -10,7 +10,7 @@ import android.support.annotation.NonNull;
 import org.justinrogers.sunshine.sync.SunshineSyncAdapter;
 
 /**
- * Provider for a widget showing today's weather.
+ * Provider for a horizontally expandable widget showing today's weather.
  *
  * Delegates widget updating to {@link TodayWidgetIntentService} to ensure that
  * data retrieval is done on a background thread
